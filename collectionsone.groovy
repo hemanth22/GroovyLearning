84a77bf1ebf1b863dab7b8b95ec374c573abd923
Hemanth B <hemanth22hemu@gmail.com>
2018-08-30 11:31:18 +0530

    Add files via upload

---
+++ collectionsone.groovy
@@ -0,0 +1,5 @@
+def beatles = ["John","Paul","George","Ringo"]
+for (int i=0; i<beatles.size() ;i++){
+    def greeting = "Hello, "
+    println "$greeting" + "$beatles[i]"
+}
---
+++ collectionstwo.groovy
@@ -0,0 +1,6 @@
+def beatles = ["John","Paul","George","Ringo"]
+for (int i=0; i<beatles.size() ;i++){
+    def greeting = "Hello, "
+    println "$greeting" + "${beatles[i]}"
+    println "${i*10}"
+}
