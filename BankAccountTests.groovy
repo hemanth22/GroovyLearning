b8bdf51e08b5c7725d6b009ecebc5975c7383e07
hemanth22 <hemanth22hemu@gmail.com>
2018-09-08 22:00:01 +0530

    Restoring test case value

--- BankAccountTests.groovy
+++ BankAccountTests.groovy
@@ -5,7 +5,7 @@ class BankAccountTests extends GroovyTestCase {
 	def void testCanDepositMoney() {
 	    def account = new BankAccount(10)
 	    account.deposit(5)
-	    assert 25 == account.balance
+	    assert 15 == account.balance
 	}
 
 }
