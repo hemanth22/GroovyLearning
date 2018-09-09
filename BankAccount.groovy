7d802558af23b7f7098cddcfd9f2e3630c2f02ff
hemanth22 <hemanth22hemu@gmail.com>
2018-09-09 12:04:12 +0530

    Bank static Interest remove

--- BankAccount.groovy
+++ BankAccount.groovy
@@ -19,7 +19,7 @@ class BankAccount {
 	def void accrueInterest() {
 		def service = new InterestRateService()
                 // def rate = service.getInterestRate()
-		def rate = 0.10
+	        //	def rate = 0.10
 		// example: 50 = 500 * 0.10
 		def accruedInterest = balance * rate
 
