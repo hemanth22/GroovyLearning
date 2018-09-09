82bd642ddd4eebbdb971e9ac5083f8942637f988
hemanth22 <hemanth22hemu@gmail.com>
2018-09-09 12:10:44 +0530

    Correcting error

--- BankAccount.groovy
+++ BankAccount.groovy
@@ -1,25 +1,25 @@
 class BankAccount {
 	private balance
-	
-	BankAccount(openingBalance){
+
+	BankAccount(openingBalance) {
 		balance = openingBalance
 	}
-	
+
 	def void deposit(amount) {
 		balance += amount
 	}
-	
+
 	def void withdraw(amount) {
-	    if (amount > balance)
+		if (amount > balance)
 			throw new InsufficientFundsException()
 
 		balance -= amount
-	}	
-	
+	}
+
 	def void accrueInterest() {
 		def service = new InterestRateService()
-                def rate = service.getInterestRate()
-	        //	def rate = 0.10
+		def rate = service.getInterestRate()
+
 		// example: 50 = 500 * 0.10
 		def accruedInterest = balance * rate
 
--- BankAccountTests.groovy
+++ BankAccountTests.groovy
@@ -4,47 +4,44 @@ import groovy.mock.interceptor.*
 class BankAccountTests extends GroovyTestCase {
 
 	private account
+
 	def void setUp() {
-	    account = new BankAccount(10)
-	    //println 'in setup'
+		account = new BankAccount(10)
 	}
-	
+
 	def void tearDown() {
-	    account = null
-	    //account = new BankAccount(25)
-	    //println 'in teardown'
+		account = null
 	}
 
-	def void testCanDepositMoney() {
-	    // def account = new BankAccount(10)
-	    account.deposit(5)
-	    assert 15 == account.balance
+	def void testCanDepositMoney() {		
+		account.deposit(5)
+		assert 15 == account.balance
 	}
-	
+
 	def void testCanWithdrawMoney() {
-            // def account = new BankAccount(25)
-            account.withdraw(5)
-            assert 5 == account.balance
+		account.withdraw(5)
+		assert 5 == account.balance
 	}
 
-	def void testCanNotWithdrawMoreMoneyThanBalance(){
-	    shouldFail(InsufficientFundsException) {
-	         account.withdraw(15)
-	    }
+	def void testCanNotWithdrawMoreMoneyThanBalance() {
+		shouldFail(InsufficientFundsException) {
+			account.withdraw(15)
+		}
 	}
-	
+
 	def void testCanAccrueInterest() {
-            def service = new MockFor(InterestRateService)
-	    service.demand.getInterestRate{
-		return 0.10
-	    }
-	    service.use {	
-	    account.accrueInterest()
-	    //result = balance + (balance * interestRate)
-	    //    11 = 10      + (10      *         0.10)
-	    //    11 = 10 + 1
-            //    11 = 11 
-	    asset 11 == account.balance	
-	   }
+		def service = new MockFor(InterestRateService)
+		service.demand.getInterestRate { 
+			return 0.10 
+		}
+		service.use {
+			account.accrueInterest()
+
+			// result = balance + (balance * interestRate)
+			// 	   11 = 10 + (10 * 0.10)
+			//	   11 = 10 + 1
+			//     11 = 11
+			assert 11 == account.balance
+		}	
 	}
 }
