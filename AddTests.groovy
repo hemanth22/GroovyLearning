c943f7c9164dc531c323b502f9124bbeb8c90c11
Hemanth B <hemanth22hemu@gmail.com>
2018-09-10 22:30:50 +0530

    Writing Test case for java

---
+++ AddTests.groovy
@@ -0,0 +1,12 @@
+import groovy.util.GroovyTestCase
+
+class AddTests extends GroovyTestCase {
+
+def void testAdditions() {		
+		def account = new Add;
+		account.addition(10,20)
+		assert 30 ==  account.c
+	
+	}
+
+}
