6efad21537d2af23fed87958fd0c64c1a58df755
Hemanth B <hemanth22hemu@gmail.com>
2018-09-10 23:22:43 +0530

    Trail and error

--- AddTests.groovy
+++ AddTests.groovy
@@ -5,7 +5,7 @@ class AddTests extends GroovyTestCase {
 def void testAdditions() {		
 		def account = new Add();
 		account.addition(10,20)
-		assert 30 ==  account.addition()
+		assert 30 ==  account.addition(30)
 	
 	}
 
