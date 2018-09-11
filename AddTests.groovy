b97d327ac6a072d4b0d2d729195a7bed89a3e094
Hemanth B <hemanth22hemu@gmail.com>
2018-09-11 07:17:09 +0530

    Unit test correction

--- AddTests.groovy
+++ AddTests.groovy
@@ -4,8 +4,8 @@ class AddTests extends GroovyTestCase {
 
 def void testAdditions() {		
 		def account = new Add();
-		account.addition(10,20)
-		assert 30 ==  account.addition(30)
+		
+		assert 30 ==  account.addition(20,10)
 	
 	}
 
