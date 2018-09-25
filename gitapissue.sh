844be1bdc3378bfbbd10ba4ae6c2ec9b23b4c608
Hemanth B <hemanth22hemu@gmail.com>
2018-09-25 11:25:24 +0530

    replace with key

--- gitapissue.sh
+++ gitapissue.sh
@@ -1,1 +1,1 @@
-curl -i -H "Authorization: token 2aa5c128fb521c9a339f0e6da5aeae9ab095faec" -d '{"title": "Travis Build no. $TRAVIS_BUILD_ID failure ", "body": "$TRAVIS_TEST_RESULT", "labels": ["critical"]}' https://api.github.com/repos/hemanth22/GroovyLearning/issues
+curl -i -H "Authorization: token key" -d '{"title": "Travis Build no. $TRAVIS_BUILD_ID failure ", "body": "$TRAVIS_TEST_RESULT", "labels": ["critical"]}' https://api.github.com/repos/hemanth22/GroovyLearning/issues
