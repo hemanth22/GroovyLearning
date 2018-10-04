bb78f1ce10f74c419951cd443befb7e6f6e8c858
Hemanth B <hemanth22hemu@gmail.com>
2018-10-04 22:16:22 +0530

    travis support workaround 2

--- gitapissue.sh
+++ gitapissue.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-curl -i -H "Authorization: token $TicketApi" -d '{"title": "Travis Build no. $TRAVIS_BUILD_NUMBER failure ", "body": "$TRAVIS_TEST_RESULT", "labels": ["critical"]}' https://api.github.com/repos/hemanth22/GroovyLearning/issues
+body="{ \"title\": \"Travis Build no. $TRAVIS_BUILD_ID failure \", \"body\": \"$TRAVIS_TEST_RESULT\", \"labels\": [\"critical\"]}"
+
+curl -i \
+     -H "Travis-API-Version: 3" \
+     -H "Authorization: token $TicketApi" \
+     -d "$body" \
+     https://api.github.com/repos/hemanth22/GroovyLearning/issues
