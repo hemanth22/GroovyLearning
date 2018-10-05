c8c7973ae3f98eb8397fc17c8e3fbd5d79113b95
Hemanth B <hemanth22hemu@gmail.com>
2018-10-05 15:37:13 +0530

    Travis support fix #3

--- gitapissue.sh
+++ gitapissue.sh
@@ -2,7 +2,6 @@
 body="{ \"title\": \"Travis Build no. $TRAVIS_JOB_NUMBER failure \", \"body\": \"Error code: $TRAVIS_TEST_RESULT \n\n JOB URL : $TRAVIS_JOB_WEB_URL\", \"labels\": [\"critical\"]}"
 
 curl -i \
-     -H "Travis-API-Version: 3" \
      -H "Authorization: token $TicketApi" \
      -d "$body" \
      https://api.github.com/repos/hemanth22/GroovyLearning/issues
