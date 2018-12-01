b4f1f47e79853b0e0f2c2f8a9e4c2cc1f9af3000
Hemanth B <hemanth22hemu@gmail.com>
2018-12-01 14:32:04 +0530

    Update AddTests.groovy

--- src/test/groovy/AddTests.groovy
+++ src/test/groovy/AddTests.groovy
@@ -1,11 +1,8 @@
 import groovy.util.GroovyTestCase
-
 class AddTests extends GroovyTestCase {
-
 def void testAdditions() {		
 		def account = new Add();
 		assert 30 ==  account.addition(20,10)
 	
 	}
-
 }
