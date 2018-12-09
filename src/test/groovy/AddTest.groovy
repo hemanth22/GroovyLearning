37f3a455ce4603f262bbe4871e93f950574cb5eb
Hemanth B <hemanth22hemu@gmail.com>
2018-12-09 12:54:10 +0530

    Update and rename AddTests.groovy to AddTest.groovy

--- src/test/groovy/AddTest.groovy
+++ src/test/groovy/AddTest.groovy
@@ -1,5 +1,5 @@
 import groovy.util.GroovyTestCase
-class AddTests extends GroovyTestCase {
+class AddTest extends GroovyTestCase {
 def void testAdditions() {		
 		def account = new Add();
 		assert 30 ==  account.addition(20,10)
